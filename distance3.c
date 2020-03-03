a0a7bf001c32e3f1966ec6405c322e0d23ddbb9d
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-03 10:41:52 +0000

    distance3.c initial commit

---
+++ distance3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+struct store
+{
+ float x;
+ float y;
+};
+typedef struct store Store;
+Store input()
+{
+ Store p;
+ printf("Enter the coordinates of a point (x,y)\n");
+ scanf("%f%f",&p.x,&p.y);
+ return p;
+}
+float process(Store p1,Store p2)
+{
+ float d;
+ d=sqrt(pow((p2.x-p1.x),2)+pow((p2.y-p1.y),2));
+ return d;
+}
+void output(Store p1,Store p2,float d)
+{
+ printf("The distance between the 2 points (%f,%f) and (%f,%f) is %f\n",p1.x,p1.y,p2.x,p2.y,d);
+}
+int main()
+{    
+    store s1,s2;
+    float d,x1,y1,x2,y2;
+    s1=input();
+    s2=input();
+    d=process(s1,s2);
+    output(s1,s2,d);
+    return 0;
+}
