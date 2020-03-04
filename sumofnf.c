09e4f9cd4cc59e8dcc351dac9e24755af34ea9e5
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-04 19:49:01 +0000

    sumofnf.c modified commit 6

--- sumofnf.c
+++ sumofnf.c
@@ -19,19 +19,6 @@ void frac_input(int n,Frac p[n])
 	for(i=0;i<n;i++)
 		scanf("%d/%d",&p[i].n,&p[i].d);
 }
-Frac process(int n,Frac f[n])
-{
-	int i;
-	Frac s;
-	s.n=f[0].n;
-	s.d=f[0].d;
-	for(i=1;i<n;i++)
-	{
-		s.n=(s.n*f[i].d)+(s.d*f[i].n);
-		s.d=s.d*f[i].d;
-	}
-	return s;
-}
 int gcd(Frac s) 
 {	    
 	int a,b,t;
@@ -54,22 +41,35 @@ Frac reduce(Frac s)
 	a.d=s.d/g;
 	return a;
 }
-void output(int n, Frac f[n],Frac s,Frac sf)
+Frac process(int n,Frac f[n])
+{
+	int i;
+	Frac s,sf;
+	s.n=f[0].n;
+	s.d=f[0].d;
+	for(i=1;i<n;i++)
+	{
+		s.n=(s.n*f[i].d)+(s.d*f[i].n);
+		s.d=s.d*f[i].d;
+	}
+	sf=reduce(s);
+	return sf;
+}
+void output(int n, Frac f[n],Frac sf)
 {
 	int i;
 	printf("The sum of ");
 	for(i=0;i<n-1;i++)
 		printf("%d/%d + ",f[i].n,f[i].d);
-	printf("%d/%d = %d/%d = %d/%d\n",f[i].n,f[i].d,s.n,s.d,sf.n,sf.d);
+	printf("%d/%d = %d/%d\n",f[i].n,f[i].d,sf.n,sf.d);
 }
 int main()
 {
 	int n;
 	n=input();
-	Frac f[n],s,sf;    
+	Frac f[n],sf; 
 	frac_input(n,f);
-	s=process(n,f);
-	sf=reduce(s);
-	output(n,f,s,sf);    
+	sf=process(n,f);
+	output(n,f,sf);    
 	return 0;
 }
