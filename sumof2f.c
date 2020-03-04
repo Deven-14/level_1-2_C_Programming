4e5b6d75789075741f529ca169a8478ee96a3c98
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-04 10:03:36 +0000

    sumof2f.c modified commit 1

--- sumof2f.c
+++ sumof2f.c
@@ -19,16 +19,33 @@ Frac process(Frac p1,Frac p2)
     p3.d=p1.d*p2.d;
     return p3;
 }
-void output(Frac p1,Frac p2,Frac p3)
+Frac gcd(Frac f)
 {
-    printf("The sum of %d/%d + %d/%d = %d/%d\n",p1.n,p1.d,p2.n,p2.d,p3.n,p3.d);
+    int t;
+    Frac p;
+    p.n=f.n;
+    p.d=f.d;
+    while(p.d>0)
+    {
+        t=p.d;
+        p.d=p.n%p.d;
+        p.n=t;
+    }
+    return p;
+}
+void output(Frac p1,Frac p2,Frac p3,Frac g)
+{   
+    int n=p3.n/g.n;
+    int d=p3.d/g.n;  // can be g.n or g.d 
+    printf("The sum of %d/%d + %d/%d = %d/%d = %d/%d\n",p1.n,p1.d,p2.n,p2.d,p3.n,p3.d,g.n,g.d);
 }
 int main()
 {
-    Frac p1,p2,p3;
+    Frac p1,p2,p3,g;
     p1=input();
     p2=input();
     p3=process(p1,p2);
     output(p1,p2,p3);
+    g=gcd(p3);
     return 0;
 }
