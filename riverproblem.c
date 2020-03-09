709bf6a56071995675c726215d7d983ebdccd883
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-03-09 12:26:18 +0000

    riverproblem.c modified commit 12

--- riverproblem.c
+++ riverproblem.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 struct hobbits
 {
-    int h1;
-    int h2;
-    int h3;
-    int h4;
+    int hob1;
+    int hob2;
+    int hob3;
+    int hob4;
 };
 typedef struct hobbits Hobbits;
 struct hobbit_pairs
@@ -15,59 +15,59 @@ struct hobbit_pairs
 typedef struct hobbit_pairs Pairs;
 struct bridge_crossing
 {
-    int t1;
-    int t2;
-    int t3;
-    int t4;
-    int t5;
+    int time1;
+    int time2;
+    int time3;
+    int time4;
+    int time5;
 };
 typedef struct bridge_crossing Bridge;
 void firstpairs(int n,Hobbits h[n],int i,Pairs p1[6])
 {
-    p1[0].a=h[i].h1;
-    p1[0].b=h[i].h2;
-    p1[1].a=h[i].h1;
-    p1[1].b=h[i].h3;
-    p1[2].a=h[i].h1;
-    p1[2].b=h[i].h4;
-    p1[3].a=h[i].h2;
-    p1[3].b=h[i].h3;
-    p1[4].a=h[i].h2;
-    p1[4].b=h[i].h4;
-    p1[5].a=h[i].h3;
-    p1[5].b=h[i].h4;
+    p1[0].a=h[i].hob1;
+    p1[0].b=h[i].hob2;
+    p1[1].a=h[i].hob1;
+    p1[1].b=h[i].hob3;
+    p1[2].a=h[i].hob1;
+    p1[2].b=h[i].hob4;
+    p1[3].a=h[i].hob2;
+    p1[3].b=h[i].hob3;
+    p1[4].a=h[i].hob2;
+    p1[4].b=h[i].hob4;
+    p1[5].a=h[i].hob3;
+    p1[5].b=h[i].hob4;
 }
 void thirdpairs(int n,Hobbits h[n],int i,int j,Pairs p3[3])
 {
     if(j==0)
     {
-        p3[0].a=h[i].h1;
-        p3[0].b=h[i].h3;
-        p3[1].a=h[i].h1;
-        p3[1].b=h[i].h4;
-        p3[2].a=h[i].h3;
-        p3[2].b=h[i].h4;
+        p3[0].a=h[i].hob1;
+        p3[0].b=h[i].hob3;
+        p3[1].a=h[i].hob1;
+        p3[1].b=h[i].hob4;
+        p3[2].a=h[i].hob3;
+        p3[2].b=h[i].hob4;
     }
     else if(j==1||j==3)
     {
-        p3[0].a=h[i].h1;
-        p3[0].b=h[i].h2;
-        p3[1].a=h[i].h1;
-        p3[1].b=h[i].h4;
-        p3[2].a=h[i].h2;
-        p3[2].b=h[i].h4;
+        p3[0].a=h[i].hob1;
+        p3[0].b=h[i].hob2;
+        p3[1].a=h[i].hob1;
+        p3[1].b=h[i].hob4;
+        p3[2].a=h[i].hob2;
+        p3[2].b=h[i].hob4;
     }
     else
     {
-        p3[0].a=h[i].h1;
-        p3[0].b=h[i].h2;
-        p3[1].a=h[i].h1;
-        p3[1].b=h[i].h3;
-        p3[2].a=h[i].h2;
-        p3[2].b=h[i].h3;
+        p3[0].a=h[i].hob1;
+        p3[0].b=h[i].hob2;
+        p3[1].a=h[i].hob1;
+        p3[1].b=h[i].hob3;
+        p3[2].a=h[i].hob2;
+        p3[2].b=h[i].hob3;
     }
 }
-int input_loops()
+int input_numofloops()
 {
     int n;
     scanf("%d",&n);
@@ -76,44 +76,44 @@ int input_loops()
 Hobbits input_values()
 {
     Hobbits h;
-    scanf("%d%d%d%d",&h.h1,&h.h2,&h.h3,&h.h4);
+    scanf("%d%d%d%d",&h.hob1,&h.hob2,&h.hob3,&h.hob4);
     return h;
 }
 int time_5thcrossing(int n, int i,int j,int k, Bridge b, Hobbits h[n],Pairs p5)
 {
-	p5.b=b.t4;
+	p5.b=b.time4;
     if(j==0)
     {
         if(k==0)
-            p5.a=h[i].h4;
+            p5.a=h[i].hob4;
         else if(k==1)
-            p5.a=h[i].h3;
+            p5.a=h[i].hob3;
         else
-            p5.a=h[i].h1;
+            p5.a=h[i].hob1;
     }
     else if(j==1 || j==3)
     {
         if(k==0)
-            p5.a=h[i].h4;
+            p5.a=h[i].hob4;
         else if(k==1)
-            p5.a=h[i].h2;
+            p5.a=h[i].hob2;
         else
-            p5.a=h[i].h1;
+            p5.a=h[i].hob1;
     }
     else
     {
         if(k==0)
-            p5.a=h[i].h3;
+            p5.a=h[i].hob3;
         else if(k==1)
-            p5.a=h[i].h2;
+            p5.a=h[i].hob2;
         else
-            p5.a=h[i].h1;
+            p5.a=h[i].hob1;
     }
     if(p5.a>p5.b)
-        b.t5=p5.a;
+        b.time5=p5.a;
     else
-        b.t5=p5.b;
-    return b.t5;
+        b.time5=p5.b;
+    return b.time5;
 }
 int check_shortest_time(int m,int timevalues_eachloop[100])  //y is it necessary to put int n before int a[n]
 {
@@ -135,15 +135,15 @@ void process(int n,Hobbits h[n],Pairs p1[6],Pairs p3[3],Pairs p5,Bridge b,int ou
         firstpairs(n,h,i,p1);
         for(j=0;j<6;j++)
         {
-            b.t1=p1[j].b;
-            b.t2=p1[j].a;   // because the larger time taking
+            b.time1=p1[j].b;
+            b.time2=p1[j].a;   // because the larger time taking
             thirdpairs(n,h,i,j,p3);
             for(k=0;k<3;k++)
             {
-                b.t3=p3[k].b;
-                b.t4=(p3[k].b<=p3[k].a)?((p3[k].b<=b.t1)?p3[k].b:b.t1):((p3[k].a<=b.t1)?p3[k].a:b.t1); //because we have to check the smallest one of the 3 who have reached the other side and not b1,b2,b2,coz b2 not necessarily go in the third pair
-                b.t5=time_5thcrossing(n,i,j,k,b,h,p5);
-                sum=b.t1+b.t2+b.t3+b.t4+b.t5;
+                b.time3=p3[k].b;
+                b.time4=(p3[k].b<=p3[k].a)?((p3[k].b<=b.time1)?p3[k].b:b.time1):((p3[k].a<=b.time1)?p3[k].a:b.time1); //because we have to check the smallest one of the 3 who have reached the other side and not b1,b2,b2,coz b2 not necessarily go in the third pair
+                b.time5=time_5thcrossing(n,i,j,k,b,h,p5);
+                sum=b.time1+b.time2+b.time3+b.time4+b.time5;
                 timevalues_eachloop[l]=sum;
                 l++;
             }
@@ -162,7 +162,7 @@ void output(int n,int all_outputs[n])
 int main()
 {
     int n;
-    n=input_loops();
+    n=input_numofloops();
     int output_eachloop[n];
     Hobbits h[n];
     Pairs p1[6],p3[3],p5;
