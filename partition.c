b31b26ee7beafe88926ab2e3583b97b5dc6d183c
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-04-05 11:25:39 +0000

    partition.c cst4 with swap commit 11

--- partition.c
+++ partition.c
@@ -21,27 +21,34 @@ void swap(int *a,int *b)
 void partition(int n, int a[n])
 {
     int i=1,j=n-1;
+    while(a[j]>a[0])
+    {
+        j--;
+    }
+     while(j>i && a[i]<=a[0])
+    {
+        i++;
+    }
     while(j>i)
     {
+        swap(&a[i],&a[j]);// where swap is placed also matters...
+        i++;
+        j--;
+        if(i>=j)
+            break;
         while(a[j]>a[0]) //"the last element gets swapped twice(last ele and pivot is one of the 2 cases)--allowed"
         {
             j--;
         }
-         while(i<j && a[i]<=a[0]) //smaller element is swapped last, so j has to be found first.....order of finding j and i matters....
+         while(a[i]<=a[0]) //smaller element is swapped last, so j has to be found first.....order of finding j and i matters....
         {
             i++;
-        }
-        if(j>i)
-        {
-            swap(&a[i],&a[j]);
-            i++;
-            j--;
         }
     }
     if(i==j && a[j]<a[0])// last element can checked twice and swapped twice
         swap(&a[i],&a[0]);
     else
-        swap(&a[i-1],&a[0]);
+        swap(&a[i-1],&a[0]);// for i>j
 }
 void output(int n, int a[n])
 {
