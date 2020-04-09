2a0bc4d7e7fe5f230ee54398c4f716a590b96cef
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-04-09 12:27:27 +0000

    qsort.c part a commit 1

---
+++ qsort.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+int input_no_of_ele()
+{
+    int a;
+    printf("Enter the number of elements:\n");
+    scanf("%d",&a);
+    return a;
+}
+void input_ele(int n,int a[n])
+{
+    printf("Enter the elements of the array:\n");
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+}
+int cmpint(const void *x,const void *y)
+{
+    if(*(int *)x>*(int *)y)
+        return 1;
+    else if(*(int *)x<*(int *)y)
+        return -1;
+    else
+        return 0;
+}
+void output(int n, int a[n])
+{
+    printf("The array elements after qsort are:\n");
+    for(int i=0;i<n;i++)
+        printf("%d ",a[i]);
+}
+int main()
+{
+    int n;
+    n=input_no_of_ele();
+    int a[n];
+    input_ele(n,a);
+    qsort(&a[0],n,sizeof(a[0]),cmpint);
+    output(n,a);
+    return 0;
+}
