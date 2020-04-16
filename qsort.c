982fc6a045c7d2699734583d6a15420fa2faf588
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-04-16 11:35:22 +0000

    qsort.c part c commit 4

--- qsort.c
+++ qsort.c
@@ -10,12 +10,11 @@ int input_no_of_str()
 }
 void input_ele(int n,char *s[n])
 {
-	char p[n][50];
+	for(int i=0;i<n;i++)
+		s[i]=(char *)malloc(50*sizeof(char));// commit 3 won't work as p is a local variable and it's data gets removed and changed to garbage after that function is done with execution and the program moves to the next function
     printf("Enter the strings:\n");
-	for(int i=0;i<n;i++)
-		scanf("%s[^\n]",p[i]);
-	for(int i=0;i<n;i++)
-		s[i]=p[i];
+    for(int i=0;i<n;i++)
+        scanf("%s",s[i]);
 }
 int cmpstr(const void *x,const void *y)
 {
@@ -26,6 +25,11 @@ void output(int n,char *s[n])
 	printf("The array elements after qsort are:\n");
 	for(int i=0;i<n;i++)
 		puts(s[i]);
+	for(int i=0;i<n;i++)
+	{
+		free(s[i]);
+		s[i]=NULL;
+	}
 }
 int main()
 {
@@ -33,7 +37,7 @@ int main()
     n=input_no_of_str();
     char *s[n];                                                                                            //s is the name of the pointer as well as the string name, that's y 1st argument in qsort is &s[0] and in puts we no need to use *s[i], just s[i]
     input_ele(n,s);
-    qsort(&s[0],n,sizeof(char *),cmpstr);                                                                  // sizeof(char *) is same as sizeof(s[0]), and first argument can be &s[0] or s
+    qsort(s,n,sizeof(char *),cmpstr);                                                                  // sizeof(char *) is same as sizeof(s[0]), and first argument can be &s[0] or s
     output(n,s);
     return 0;
 }
