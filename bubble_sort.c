0dc62d2bea61600cff97983e41a6cade1d72c023
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-04-18 13:47:11 +0000

    bubble_sort.c commit 3

--- bubble_sort.c
+++ bubble_sort.c
@@ -20,20 +20,20 @@ int cmpint(const void *x,const void *y)
 void bubblesort(void *base,size_t nmemb,size_t size,int (*compare)(const void*,const void*))
 {
 	int i,j,x,k;
-	char temp;
+	char temp;                                                                                       //since we are swapping bit by bit we don't need char* only char is enough
 	for(i=0;i<nmemb-1;i++)
 	{
 		for(j=0;j<nmemb-i-1;j++)
 		{
-			x=compare((base+(size*j)),(base+(size*j)+1));                                            //(base+j)and(base+j+1)is wrong as the void pointer won't know after how many bits is the next one
+			x=compare((base+(size*j)),(base+(size*(j+1))));                                          //(base+j)and(base+j+1)is wrong as the void pointer won't know after how many bits is the next one
 			if(x>0)                                                                                  //i.e if *(base+i) is greater
 			{                                                                                        //cannot dereference a void pointer obviously because it won't know how many bits of data to accessed, we have to type cast to dereference
 				k=0;
 				while(k!=size)                                                                       //swapping each bit untill it the required bit size is reached
 				{
 					temp=*((char*)base+(size*j)+k);                                                  //*********not*(char*)(base+j+k),but*((char*)base+j+k)
-					*((char*)base+(size*j)+k)=*((char*)base+(size*j)+1+k);
-					*((char*)base+(size*j)+1+k)=temp;
+					*((char*)base+(size*j)+k)=*((char*)base+(size*(j+1))+k);
+					*((char*)base+(size*(j+1))+k)=temp;
 					k++;
 				}
 			}
@@ -42,7 +42,7 @@ void bubblesort(void *base,size_t nmemb,size_t size,int (*compare)(const void*,c
 }
 void output(int n, int a[n])
 {
-    printf("The array elements after qsort are:\n");
+    printf("The array elements after bubblesort are:\n");
     for(int i=0;i<n;i++)
         printf("%d ",a[i]);
 }
