11fc3b1108398caf2f1d798b7b402cf98f4329a5
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2020-05-26 12:08:53 +0000

    singly_linked_list.h commit 2

--- singly_linked_list.h
+++ singly_linked_list.h
@@ -1,19 +1,35 @@
+#ifndef LIST_H
+#define LIST_H
+
 typedef struct node
 {
 	void *data;
 	struct node *next;
 }Node;
+typedef struct list
+{
+	Node *head;
+	Node *tail;
+	unsigned int count;
+}List;
 
-Node* new_node(void *val);
-void input_nodes(Node **head,void *b);
-void push_beg(Node **head,void *val);
-void push_end(Node **head,void *val);
-void insertafter_specfic_node(Node *prev_node,void *val);
-void insert_at_specfic_index(Node **head,int ind,void *val);
-void* pop_beg(Node **head);
-void* pop_end(Node **head);
-void pop_specfic_val(Node **head,void *val);
-void pop_by_index(Node **head,int ind);
-void process(Node **head,void *a);
-void print_list(Node *head);
-void free_list(Node *head);
+Node* create_node(void *val);
+List* create_list();
+void input_list(List *l,int a,int b[]);//should change typecast for different datatypes
+void add_head(List *l,void *val);
+void add_tail(List *l,void *val);
+void insert_after_specfic_node(List *l,Node *n,void *val);
+void insert_at_specfic_index(List *l,int ind,void *val);
+void* pop_head(List *l);
+void* pop_tail(List *l);
+void pop_specfic_val(List *l,void *val);//should change typecast for different datatypes
+void pop_by_index(List *l,int ind);
+void print_list(List *l);//should change typecast for different datatypes
+void output_count(int c);
+void list_empty(List *l);
+void list_delete(List *l);
+
+void input_fuctions_trial(List *l,int *b,int d);
+void deleting_fuctions_trial(List *l,int *c);
+
+#endif
