983a433262f4dd559dd6f7964dc141ba6c76a897
Deven Prakash Paramaj <devenparamaj.is19@bmsce.ac.in>
2021-01-26 13:01:23 +0000

    singly_linked_list.h commit 3 (26/01/2021)

--- singly_linked_list.h
+++ singly_linked_list.h
@@ -1,35 +1,54 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef SINGLYLINKEDLIST_H
+#define SINGLYLINKEDLIST_H
 
-typedef struct node
+#include <stdbool.h>
+
+struct node
 {
 	void *data;
 	struct node *next;
-}Node;
-typedef struct list
+};
+typedef struct node Node;
+
+struct iterator;
+
+struct singlyLinkedList
 {
 	Node *head;
 	Node *tail;
-	unsigned int count;
-}List;
+	size_t sizeOfDataType;
+	struct iterator *i;//to avoid dangling pointer in iterator when i free the singly linked list
+};
+typedef struct singlyLinkedList SinglyLinkedList;
 
-Node* create_node(void *val);
-List* create_list();
-void input_list(List *l,int a,int b[]);//should change typecast for different datatypes
-void add_head(List *l,void *val);
-void add_tail(List *l,void *val);
-void insert_after_specfic_node(List *l,Node *n,void *val);
-void insert_at_specfic_index(List *l,int ind,void *val);
-void* pop_head(List *l);
-void* pop_tail(List *l);
-void pop_specfic_val(List *l,void *val);//should change typecast for different datatypes
-void pop_by_index(List *l,int ind);
-void print_list(List *l);//should change typecast for different datatypes
-void output_count(int c);
-void list_empty(List *l);
-void list_delete(List *l);
+struct iterator
+{
+	Node *curr;
+	struct singlyLinkedList *s;
+};
+typedef struct iterator Iterator;
 
-void input_fuctions_trial(List *l,int *b,int d);
-void deleting_fuctions_trial(List *l,int *c);
+Node* newNode(size_t, void *);
+SinglyLinkedList* newSinglyLinkedList(size_t);//size_t for sizeOfDataType
+
+void addHead(SinglyLinkedList *, void *);
+void addTail(SinglyLinkedList *, void *);
+
+void removeHead(SinglyLinkedList *);
+void removeTail(SinglyLinkedList *);
+void removeSpecificNode(SinglyLinkedList *, void *);
+
+bool search(SinglyLinkedList *, void *);
+
+SinglyLinkedList* freeSinglyLinkedList(SinglyLinkedList *);
+
+Iterator* newIterator(SinglyLinkedList *);
+
+extern void start(Iterator *);//extern coz i have used inline function
+extern bool end(Iterator *);
+extern void next(Iterator *);
+extern void* data(Iterator *);
+
+Iterator* freeIterator(Iterator *);
 
 #endif
