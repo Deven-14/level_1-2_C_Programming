0e508ae6f3f15d4d86a1a39b453d837c8533e69d
Deven-14 <deven.paramaj@gmail.com>
2021-02-05 15:31:17 +0530

    added ctest

---
+++ singlyLLTest/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/deven/level1-2/singlyLLTest
+# Build directory: /home/deven/level1-2/singlyLLTest/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lib")
+subdirs("tests")
---
+++ singlyLLTest/build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/deven/level1-2/singlyLLTest/tests
+# Build directory: /home/deven/level1-2/singlyLLTest/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(singlyLLTest "/home/deven/level1-2/singlyLLTest/build/bin/tests")
+set_tests_properties(singlyLLTest PROPERTIES  _BACKTRACE_TRIPLES "/home/deven/level1-2/singlyLLTest/tests/CMakeLists.txt;9;add_test;/home/deven/level1-2/singlyLLTest/tests/CMakeLists.txt;0;")
