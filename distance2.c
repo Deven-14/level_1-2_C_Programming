e2c81fbf68a31c1fa52fd2e864eada89acfb1fc4
Deven-14 <deven.paramaj@gmail.com>
2022-07-20 19:28:41 +0530

    made program more readable

--- distance2.c
+++ distance2.c
@@ -1,37 +1,44 @@
 #include<stdio.h>
 #include<math.h>
-float input1()
+
+float input_x_coordinate_of_ith_point(int i)
 {
     float x;
-    printf("Enter the x-coordinates of a point\n");
-    scanf("%f",&x);
+    printf("Enter the x-coordinates of a point %d: ", i);
+    scanf("%f", &x);
     return x;
 }
-float input2()
+
+float input_y_coordinate_of_ith_point(int i)
 {
     float y;
-    printf("Enter the y-coordinates of a point\n");
-    scanf("%f",&y);
+    printf("Enter the y-coordinates of a point %d: ", i);
+    scanf("%f", &y);
     return y;
 }
-float process(float x1,float y1,float x2,float y2)
+
+float calculate_distance(float x1, float y1, float x2, float y2)
 {
-    float d;
-    d=sqrt(pow((x2-x1),2)+pow((y2-y1),2));
-    return d;
+    float distance;
+    distance = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+    return distance;
 }
-void output(float x1,float y1,float x2,float y2,float d)
+
+void print_distance(float x1, float y1, float x2, float y2, float distance)
 {
-    printf("The distance between the 2 points (%f,%f) and (%f,%f) is %f\n",x1,y1,x2,y2,d);
+    printf("The distance between the 2 points (%f, %f) and (%f, %f) is %f\n", x1, y1, x2, y2, distance);
 }
+
 int main()
 {
-    float d,x1,y1,x2,y2;
-    x1=input1();
-    y1=input2();
-    x2=input1();
-    y2=input2();
-    d=process(x1,y1,x2,y2);
-    output(x1,y1,x2,y2,d);
+    float x1, x2, y1, y2, distance;
+    x1 = input_x_coordinate_of_ith_point(1);
+    y1 = input_y_coordinate_of_ith_point(1);
+    x2 = input_x_coordinate_of_ith_point(2);
+    y2 = input_y_coordinate_of_ith_point(2);
+
+    distance = calculate_distance(x1, y1, x2, y2);
+
+    print_distance(x1, y1, x2, y2, distance);
     return 0;
 }    
