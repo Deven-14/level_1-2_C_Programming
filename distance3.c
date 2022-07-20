c6bc7cc78fc62b4c869e1d0f68758567b0add5f1
Deven-14 <deven.paramaj@gmail.com>
2022-07-20 19:34:26 +0530

    made program more readable

--- distance3.c
+++ distance3.c
@@ -1,35 +1,40 @@
 #include<stdio.h>
 #include<math.h>
-struct store
+
+struct point
 {
- float x;
- float y;
+    float x;
+    float y;
 };
-typedef struct store Store;
-Store input()
+typedef struct point Point;
+
+Point input_ith_point(int i)
 {
- Store p;
- printf("Enter the coordinates of a point (x,y)\n");
- scanf("%f%f",&p.x,&p.y);
- return p;
+    Point p;
+    printf("Enter the coordinates of point %d (x, y): ", i);
+    scanf("%f %f", &p.x, &p.y);
+    return p;
 }
-float process(Store p1,Store p2)
+
+float calculate_distance(Point p1, Point p2)
 {
- float d;
- d=sqrt(pow((p2.x-p1.x),2)+pow((p2.y-p1.y),2));
- return d;
+    float distance;
+    distance = sqrt(pow((p2.x - p1.x), 2) + pow((p2.y - p1.y), 2));
+    return distance;
 }
-void output(Store p1,Store p2,float d)
+
+void print_distance(Point p1, Point p2, float distance)
 {
- printf("The distance between the 2 points (%f,%f) and (%f,%f) is %f\n",p1.x,p1.y,p2.x,p2.y,d);
+    printf("The distance between the 2 points (%f, %f) and (%f, %f) is %f\n", p1.x, p1.y, p2.x, p2.y, distance);
 }
+
 int main()
 {    
-    Store s1,s2;
-    float d;
-    s1=input();
-    s2=input();
-    d=process(s1,s2);
-    output(s1,s2,d);
+    Point p1, p2;
+    float distance;
+    p1 = input_ith_point(1);
+    p2 = input_ith_point(2);
+    distance = calculate_distance(p1, p2);
+    print_distance(p1, p2, distance);
     return 0;
 }
